9c927222f48e33015fe11f3ccb77f975629d2fe9
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2025-11-18 11:11:00 +0500

    first commitsssssazzassa

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 // Do not end with trailing slash to avoid // when joining paths
-export const API_BASE = ((import.meta as any).env?.VITE_API_BASE || 'http://127.0.0.1:8000/api').replace(/\/$/, '');
+export const API_BASE = ((import.meta as any).env?.VITE_API_BASE || 'https://ideatorapi.cdcgroup.uz/api').replace(/\/$/, '');
 
 export type LoginResponse = {
   access: string;
